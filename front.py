3abe204de67b409e1b510f7c9421fb3af02fd0c9
Paweł Ogorzałek <pawel.ogorzalek@coders-mill.com>
2015-11-21 19:06:59 +0100

    static

--- front.py
+++ front.py
@@ -1,9 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 front_blueprint = Blueprint('front', __name__)
 
 
 @front_blueprint.route('')
 def landing():
-    return 'landing'
+    return render_template('index.html')
 
+
+@front_blueprint.route('login')
+def login():
+    return render_template('login.html')
