3c99978656b6d8805b8e5da50846e6d6268db58a
drootnar <drootnar@gmail.com>
2015-11-21 19:05:50 +0100

    Models and some endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
-from models import *
+from service import *
 
 
 @app.route('/')
@@ -19,6 +19,28 @@ def hello():
 def hello_name(name):
     return "Hello {}!".format(name)
 
+
+# All rooms
+@app.route('/room', methods=['GET'])
+def room_get():
+    service = RoomService(db)
+    data = {
+        'major': request.args.get('major', None),
+        'minor': request.args.get('minor', None),
+    }
+    rooms = service.get(data)
+    return jsonify({
+        'results': [room.__json__() for room in rooms],
+    })
+
+
+# Add room
+@app.route('/room', methods=['POST'])
+def room_post():
+    service = RoomService(db)
+    room = service.add(request.json)
+    # return jsonify(room.__json__())
+    return 'OK'
 
 if __name__ == '__main__':
     app.run()
