25b815249790885ed2f310bc498152c738145207
drootnar <drootnar@gmail.com>
2015-11-21 12:57:42 +0100

    Database ready

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 import os
+
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+db = SQLAlchemy(app)
+
+from models import *
 
 
 @app.route('/')
 def hello():
-    return os.environ['APP_SETTINGS']
+    return "Hello World!"
 
 
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
 
 if __name__ == '__main__':
     app.run()
