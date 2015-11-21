22c26609426c2a426e08b25d27314adcb452225b
drootnar <drootnar@gmail.com>
2015-11-21 12:29:05 +0100

    Second commit

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "Hello beacon world!"
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
 
 if __name__ == '__main__':
     app.run()
