905cbc6ce102f8b58a716bcf54d39d12f878f1f3
Paweł Ogorzałek <pawel.ogorzalek@coders-mill.com>
2015-11-22 03:11:22 +0100

    fixes

--- app/static/app/controllers/room_index_ctrl.js
+++ app/static/app/controllers/room_index_ctrl.js
@@ -10,6 +10,7 @@ angular.module('app').controller('RoomIndexController', ['$scope', '$log', 'Room
 
     $scope.fetch_page = function(data) {
         $scope.rooms = data.results;
+        $scope.set_current_room($scope.rooms[0].id);
     };
 
     $scope.set_current_room = function(id) {
